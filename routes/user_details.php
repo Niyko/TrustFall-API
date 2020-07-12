1935b99447acd974cef9a8b1ecd51d4794906156
Niyko <creatyfication@gmail.com>
2020-07-12 12:06:33 +0530

    Added user details API

---
+++ routes/user_details.php
@@ -0,0 +1,33 @@
+<?php
+//Load all essential framework compoents
+require_once("../framework/config.php");
+
+//User given details
+$mobile = $_POST["mobile"];
+$password = $_POST["password"];
+
+//Check if user exist
+$existing_user = $db->where("mobile", $mobile)->getOne("users");
+if(!is_null($existing_user)){
+    if($existing_user['password']==$password){
+        print_json([
+            "status" => true,
+            "code" => "user-found",
+            "user_details" => $existing_user
+        ]);
+    }
+    else {
+        print_json([
+            "status" => false,
+            "code" => "user-pass-incorrect",
+            "description" => "Password of this user didn't match"
+        ]);
+    }
+}
+else {
+    print_json([
+        "status" => false,
+        "code" => "user-non-exist",
+        "description" => "This user does not exist"
+    ]);
+}
