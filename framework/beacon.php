c707d5659d8982acbf9b16873bc01ca1c6b6b912
Niyko <niyko.official@gmail.com>
2020-11-12 19:28:29 +0530

    fixed sms issue

--- framework/beacon.php
+++ framework/beacon.php
@@ -22,6 +22,7 @@ class Beacon {
 
 	//Sending CURL request to MSG91 API
 	private function sendAPI($mobile_no, $varname){
+		global $txtlocal_auth_key;
 		$numbers = array($mobile_no);
     	$sender = urlencode('TRSFAL');
     	$numbers = implode(',', $numbers);
@@ -31,7 +32,7 @@ class Beacon {
 			'TemplateName' => 'Trustfall', 
 			'VAR1' => $varname
 		);
-    	$ch = curl_init('http://2factor.in/API/V1/390135f1-2461-11eb-83d4-0200cd936042/ADDON_SERVICES/SEND/TSMS');
+    	$ch = curl_init('http://2factor.in/API/V1/'.$txtlocal_auth_key.'/ADDON_SERVICES/SEND/TSMS');
     	curl_setopt($ch, CURLOPT_POST, true);
     	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
